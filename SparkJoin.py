6e69541fb97dc7de6d4b3936a3f6938835de7bbd
rlsalcido24 <rlsalcido24>
2021-07-23 20:35:10 +0000

    sparkjoin

---
+++ SparkJoin.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+df1 = spark.sql("select * from roberto.testfinrec")
+df2 = spark.sql("select * from roberto.testfinrec2")
+df1rp = df1.repartition(2000, "join_id")
+df2rp = df2.repartition(2000, "join_id")
+display(df1rp)
+display(df2rp)
+
+# COMMAND ----------
+
+dfjoin = df1rp.join(df2rp,df1rp.join_id ==  df2rp.join_id,"inner") 
+display(dfjoin)
