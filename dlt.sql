44fb92fee3cd39fb78de51d3998a3f03b63b1f1c
rlsalcido24 <rlsalcido24>
2022-09-18 23:51:07 +0000

    abc
    abc

---
+++ dlt.sql
@@ -0,0 +1,23 @@
+-- Databricks notebook source
+
+CREATE LIVE TABLE customers
+COMMENT "The customers buying finished products, ingested from /databricks-datasets."
+TBLPROPERTIES ("quality" = "mapping")
+AS SELECT * FROM robertotpch.customers c
+order by c.customer_key
+
+-- COMMAND ----------
+
+CREATE LIVE TABLE nations
+COMMENT "Nations"
+TBLPROPERTIES ("quality" = "mapping")
+AS SELECT * FROM robertotpch.nations n
+order by n.nation_key
+
+-- COMMAND ----------
+
+CREATE LIVE TABLE orders
+COMMENT "Orders"
+TBLPROPERTIES ("quality" = "mapping")
+AS SELECT * FROM robertotpch.orders o
+order by o.order_date
